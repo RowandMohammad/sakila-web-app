7ffb5b36eec98e1a42c84114997a12666d43378e
Rowand Mohammad <rowandnajim@gmail.com>
2023-09-28 17:00:18 +0100

    Fixing code smells

--- src/main/java/com/sakila/sakilawebapp/controller/ActorController.java
+++ src/main/java/com/sakila/sakilawebapp/controller/ActorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/actors")
 public class ActorController {
